c1a4459eaa044cd015ea38ddc9d986e3239d5cba
ohyubun <ohyubun@gmail.com>
2017-08-18 16:49:19 +0800

    rename

--- AgvJson/Program.cs
+++ AgvJson/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AgvJson
 {
@@ -10,6 +11,22 @@ namespace AgvJson
     {
         static void Main(string[] args)
         {
+            StatusChangeReport statusReport = new StatusChangeReport
+            {
+                Area = "AREA001",
+                VehicleId = "AGV013",
+                CommandId = "",
+                Mode = "ONLINE",
+                Status = "IDLE",
+                SubStatus = "",
+                TransactionNo = 1,
+                VehicleDirection = 0.0134
+            };
+            string jsonStatus = JsonConvert.SerializeObject(statusReport);
+
+
+            Console.WriteLine(jsonStatus);
+            Console.ReadKey();
         }
     }
 }
