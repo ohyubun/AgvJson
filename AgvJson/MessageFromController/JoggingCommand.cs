3449f1ff3f68a72709e673c3cf69ca3c685b2af8
ohyubun <ohyubun@gmail.com>
2017-09-14 14:29:30 +0800

    remove message backcolor

---
+++ AgvJson/MessageFromController/JoggingCommand.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+/// <summary>
+/// Message Id : JoggingCommand
+/// Usage : request vehicle to execute motor jogging,Motor zeroing,home searching,moving
+/// Primary : Y
+/// Secondary : JoggingCommandAck
+/// Trigger : any
+/// Direction : VMS client-> Vehicle
+/// JSON example :
+/// 
+/// 
+/// 
+/// 
+/// 
+/// 
+/// </summary>
+
+namespace AgvJson
+{
+    public class JoggingCommand
+    {
+        /// <summary>
+        /// ['AGV'][0001...9999]
+        /// ex : 'AGV0034'
+        /// </summary>
+        public string VehicleId { get; set; }
+        /// <summary>
+        /// 'MOTOR_JOGGING'/'MOTOR_ZEROING'
+        /// ex : TRANSPORT
+        /// </summary>
+        public string CommandType { get; set; }
+        /// <summary>
+        /// 'RIGHT_WHEEL','LEFT_WHEEL','FORWARD_WHEEL','HEADING_WHEEL'
+        /// 'RIGHT_1_WHEEL','RIGHT_2_WHEEL','LEFT_1_WHEEL','LEFT_2_WHEEL'
+        /// 'LIFT_ARM','ROTATE_ARM','FORWARD_ARM','ROLLER_ARM'
+        /// if <CommandType> equals to 'MOTOR_JOGGING'/'MOTOR_ZEROING',this property is meaningful
+        /// ex : RIGHT_WHEEL
+        /// </summary>
+        public string MotorName { get; set; }
+        /// <summary>
+        /// 0~10 M/s for line speed,RPM for rotation speed
+        /// ex : 0.5
+        /// </summary>
+        public double Speed { get; set; }
+        /// <summary>
+        /// 'FORWARD','BACKWARD'
+        /// ex : FORWARD
+        /// </summary>
+        public string Direction { get; set; }
+      
+
+    }
+
+    public class JoggingCommandAck
+    {
+        /// <summary>
+        /// [0..99]
+        /// 0 : Accepted,1 : Rejected, 99 : other reason
+        /// ex : 0
+        /// </summary>
+        public int AckCode { get; set; }
+        /// <summary>
+        /// any string
+        /// '', 'Busy','Alarm'.....
+        /// ex : 'Busy'
+        /// </summary>
+        public string Reason { get; set; }
+    }
+}
---
+++ AgvJson/MessageFromController/PilotCommand.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+/// <summary>
+/// Message Id : PilotCommand
+/// Usage : request vehicle to do pilot by manual
+/// Primary : Y
+/// Secondary : PilotCommandAck
+/// Trigger : any
+/// Direction : VMS client-> Vehicle
+/// JSON example :
+/// 
+/// 
+/// 
+/// 
+/// 
+/// 
+/// </summary>
+
+namespace AgvJson
+{
+    public class PilotCommand
+    {
+        /// <summary>
+        /// ['AGV'][0001...9999]
+        /// ex : 'AGV0034'
+        /// </summary>
+        public string VehicleId { get; set; }
+        /// <summary>
+        /// 'STEERING'/'SLAM_MAPPING'/'HOME_SEARCHING'
+        /// ex : STEERING
+        /// </summary>
+        public string CommandType { get; set; }
+        /// <summary>
+        /// Valid on <CommandType> = 'STEERING'
+        /// 0~10 M/s
+        /// ex : 1.2
+        /// </summary>
+        public double Speed { get; set; }
+        /// <summary>
+        /// Valid on <CommandType> = 'STEERING'
+        /// 'FORWARD','BACKWARD','LEFT,'RIGHT'
+        /// ex : FORWARD
+        /// </summary>
+        public string Heading { get; set; }
+
+        /// <summary>
+        /// Valid on <CommandType> = 'STEERING'
+        /// it indicates the duration that vehicle keeps running
+        /// 0~10 sec
+        /// ex : 5.5
+        /// </summary>
+        public double ActionTime { get; set; }
+    }
+
+    public class PilotCommandAck
+    {
+        /// <summary>
+        /// [0..99]
+        /// 0 : Accepted,1 : Rejected, 99 : other reason
+        /// ex : 0
+        /// </summary>
+        public int AckCode { get; set; }
+        /// <summary>
+        /// any string
+        /// '', 'Busy','Alarm'.....
+        /// ex : 'Busy'
+        /// </summary>
+        public string Reason { get; set; }
+    }
+}
